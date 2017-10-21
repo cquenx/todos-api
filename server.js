4cca66783cea2579ccbd6192ce4981f2fea5d634
Chandra Challa <iamcquenx@gmail.com>
2017-10-22 02:10:51 +0530

    filter by completion status and search by description

--- server.js
+++ server.js
@@ -9,14 +9,32 @@ var todoNextId = 1;
 
 app.use(bodyParser.json());
 
+//GET
+//query parameters
 app.get('/' , function(reg, res) {
 res.send('Todo API Root')
 });
 
 app.get('/todos' , function(req, res) {
-	res.json(todos);
+var queryParams = req.query;
+	var filteredTodos = todos;
+
+	if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+		filteredTodos = _.where(filteredTodos, {completed: true});
+	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+		filteredTodos = _.where(filteredTodos, {completed: false});
+	}
+
+	if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
+	}
+
+	res.json(filteredTodos);
 });
 
+//GET /todos/:id
 app.get('/todos/:id' , function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
@@ -28,7 +46,7 @@ app.get('/todos/:id' , function(req, res) {
 		}
 });
 
-//POST
+//POST /todos
 app.post('/todos', function(req, res) {
 	var body = _.pick(req.body, 'description' , 'completed') ;
 
@@ -48,7 +66,7 @@ app.post('/todos', function(req, res) {
 	res.json(body);
 });
 
-//DELETE 
+//DELETE /todos/:id
 app.delete('/todos/:id' , function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	var matchedTodo = _.findWhere(todos, {id: todoId});
