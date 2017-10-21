a0fa2b675c233a9ad0ee3799e0b5b2d9a3778c64
Chandra Challa <iamcquenx@gmail.com>
2017-10-21 21:21:58 +0530

    DELETE todos added to API

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ app.post('/todos', function(req, res) {
 	}
 
 	body.description = body.description.trim();
-	
+
 	//add id field
 	body.id = todoNextId;
 	todoNextId++;
@@ -55,6 +55,20 @@ app.post('/todos', function(req, res) {
 
 	res.json(body);
 });
+
+//DELETE
+app.delete('/todos/:id' , function(req, res) {
+	var todoId = parseInt(req.params.id, 10);
+	var matchedTodo = _.findWhere(todos, {id: todoId});
+
+	if(!matchedTodo) {
+		res.status(404).json({"error": "no todo found with that id"})
+	} else {
+		todos = _.without(todos, matchedTodo)
+		res.json(matchedTodo);
+	}
+});
+
 
 app.listen(PORT, function () {
 	console.log('Express is running on port ' + PORT);
